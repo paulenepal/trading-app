59a996e72eb7d9600c11d48be87b89bf192c8d0c
Marc Dimacuha <dimacuha.marc@gmail.com>
2024-04-06 10:29:19 +0800

    [FEAT] Add WalletsController, User model changes, Wallet model, Create Wallets migration, and related tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,20 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
+  has_one :wallet, dependent: :destroy
+  after_create :create_wallet_after_signup
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :jwt_authenticatable, jwt_revocation_strategy: self
+
+  private
+
+  def create_wallet_after_signup
+    if role == 0
+      Wallet.create(user_id: id, balance: 50000)
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_04_125526) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_06_020126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_04_125526) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "wallets", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.decimal "balance", precision: 10, scale: 2, default: "0.0"
+    t.decimal "decimal", precision: 10, scale: 2, default: "0.0"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
