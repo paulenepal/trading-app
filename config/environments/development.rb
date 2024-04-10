2e724ddea07641c81b65e92d02f7b8ca6c9b3426
Paulene Pallan <paulenepallan@gmail.com>
2024-04-10 14:21:54 +0800

    chore: added additional config for action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,9 +34,18 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
+  # USING DEVISE
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { 
+    host: 'localhost', 
+    port: 3001 
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "localhost", 
+    :port => 1025
+  }
 
   config.action_mailer.perform_caching = false
 
@@ -70,6 +79,4 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
-
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3001 }
 end
