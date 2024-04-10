322582c443f92676cb95858f67a23d952cda5c87
Paulene Pallan <paulenepallan@gmail.com>
2024-04-10 14:19:24 +0800

    feat: added User roles, scope for user story #5, and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :jwt_authenticatable, jwt_revocation_strategy: self
+
+  enum role: { pending_trader: 0, trader: 1, admin: 2 }
+  # trader = approved trader
+  # feat: if may approved trader, do we need to include rejected traders here sa role?
+  # or covered na ng pending trader? 
+
+  scope :pending, -> { where(role: :pending_trader) } # [chore] refactor: need to include scope here na dapat ung confirmed emails lang makikita ni admin sa pending trader
+
+  validates :email, :first_name, :last_name, :birthday, presence: true
+  validates :username, presence: true, uniqueness: true
+
 end
