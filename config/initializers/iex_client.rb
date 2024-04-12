d59d8daf9fc9c7e0d2004a2f83445b147c42118d
Paulene Pallan <paulenepallan@gmail.com>
2024-04-12 23:16:00 +0800

    refactor: iex client setup [wip]

--- config/initializers/iex_client.rb
+++ config/initializers/iex_client.rb
@@ -1,5 +1,21 @@
+require 'iex-ruby-client'
+
 IEX::Api.configure do |config|
-  config.publishable_token = ENV['IEX_API_PUBLISHABLE_TOKEN'],
-  config.secret_token = ENV['IEX_API_SECRET_TOKEN'],
-  config.endpoint = 'https://cloud.iexapis.com/v1' # use 'https://sandbox.iexapis.com/v1' for Sandbox
+  config.publishable_token = ENV['IEX_API_PUBLISHABLE_TOKEN']
+  # config.secret_token = ENV['IEX_API_SECRET_TOKEN']
+  config.endpoint = 'https://cloud.iexapis.com/v1'
+  # config.endpoint = 'https://api.iex.cloud/v1'
 end
+
+IexClient = IEX::Api::Client.new(
+  endpoint: 'https://cloud.iexapis.com/v1',
+  # endpoint: 'https://api.iex.cloud/v1',
+  publishable_token: ENV['IEX_API_PUBLISHABLE_TOKEN']
+  # secret_token: ENV['IEX_CLOUD_API_SECRET_TOKEN']
+)
+
+# IEX::Api.configure do |config|
+#   config.publishable_token = Rails.application.credentials.iex[:public_key] # defaults to ENV['IEX_API_PUBLISHABLE_TOKEN']
+#   config.secret_token = Rails.application.credentials.iex[:secret_key] # defaults to ENV['IEX_API_SECRET_TOKEN']
+#   config.endpoint = 'https://cloud.iexapis.com/v1' # use 'https://sandbox.iexapis.com/v1' for Sandbox
+# end
