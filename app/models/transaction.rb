915128a87249543a1ff1eb64202bd8e0614372ad
Marc Dimacuha <dimacuha.marc@gmail.com>
2024-04-12 20:03:27 +0800

    [CHORE] Add user association and validation to Transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,6 @@
 class Transaction < ApplicationRecord
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :iex_stock_symbol, presence: true
+  validates :transaction_type, presence: true
 end
