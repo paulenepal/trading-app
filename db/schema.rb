9b13ba4df9be78693722dca6afbff53508a4fae9
Marc Dimacuha <dimacuha.marc@gmail.com>
2024-04-12 20:02:43 +0800

    [FEAT] Add transactions table to the database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_06_020126) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_12_115908) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "iex_stock_symbol", null: false
+    t.integer "transaction_type"
+    t.integer "quantity"
+    t.decimal "price", precision: 10, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
