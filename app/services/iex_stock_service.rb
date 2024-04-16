434456c45856ee94e91723e36fd1d531f59613a6
Paulene Pallan <paulenepallan@gmail.com>
2024-04-16 19:43:39 +0800

    refactor: updated iex stock service

--- app/services/iex_stock_service.rb
+++ app/services/iex_stock_service.rb
@@ -1,9 +1,11 @@
-require 'httparty'
+require 'iex-ruby-client'
 
 class IexStockService
-  def initialize(iex_client)
-    @client = iex_client
-  end
+  @client = IEX::Api::Client.new(
+    publishable_token: ENV['IEX_API_PUBLISHABLE_TOKEN'],
+    secret_token: ENV['IEX_API_SECRET_TOKEN'],
+    endpoint: 'https://cloud.iexapis.com/v1'
+)
 
   def self.fetch_quote(symbol)
     @client.quote(symbol)
