aa0adfefb95193055f0572235ba7d9d80a26cfa7
Paulene Pallan <paulenepallan@gmail.com>
2024-04-16 19:39:49 +0800

    refactor: updated watchlist [wip] - not yet working

--- app/controllers/watchlist_controller.rb
+++ app/controllers/watchlist_controller.rb
@@ -1,16 +1,20 @@
 class WatchlistController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
+
+  # [wip]
 
   def show
-    symbol = params[:symbol]
-    iex_stock_service = IexStockService.new(IexClient)
-    quote_data = iex_stock_service.fetch_quote(symbol)
+    @symbol = params[:symbol]
+    @client = IEX::Api::Client.new(
+      secret_token: ENV['IEX_API_SECRET_TOKEN'],
+      endpoint: 'https://cloud.iexapis.com/v1'
+    )
+    @quote_data = @client.quote(@symbol)
 
     if quote_data
-      render json: { latest_price: quote_data.latest_price }
+      render json: { symbol: @quote_data.symbol, latest_price: @quote_data.latest_price }
     else
-      render json: { error_message: "Failed to fetch quote for symbol #{symbol}" }, status: :not_found
+      render json: { error_message: "Failed to fetch quote for symbol #{@symbol}" }, status: :not_found
     end
   end
-  
 end
