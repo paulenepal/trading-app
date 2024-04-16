6c2d915d1101fdcb6ed255b6830b21299a52886c
Marc Dimacuha <dimacuha.marc@gmail.com>
2024-04-16 11:41:47 +0800

    [CHORE] Refactor transactions controller to include before_action and private methods

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,50 @@
 class TransactionsController < ApplicationController
+  
+  before_action :set_transaction, only: [:show, :update, :destroy]
+
+  # GET /transactions
+  def index
+    @transactions = Transaction.all
+    render json: @transactions
+  end
+
+  # GET /transactions/:id
+  def show
+    render json: @transaction
+  end
+
+  # POST /transactions
+  def create
+    @transaction = Transaction.new(transaction_params)
+    if @transaction.save
+      render json: @transaction, status: :created
+    else
+      render json: @transaction.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /transactions/:id
+  def update
+    if @transaction.update(transaction_params)
+      render json: @transaction
+    else
+      render json: @transaction.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /transactions/:id
+  def destroy
+    @transaction.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_transaction
+    @transaction = Transaction.find(params[:id])
+  end
+
+  def transaction_params
+    params.require(:transaction).permit(:user_id, :iex_stock_symbol, :transaction_type, :quantity, :price)
+  end
 end
