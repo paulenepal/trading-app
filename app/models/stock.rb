5add8ea1ee26627c0ac2574d56785a383223e529
Paulene Pallan <paulenepallan@gmail.com>
2024-04-19 22:48:53 +0800

    fix: updated to correct average buy formula and included NumberHelper to convert numbers to currency

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,6 @@
 class Stock < ApplicationRecord
+  include ActiveSupport::NumberHelper
+
   belongs_to :user
 
   scope :search_by_symbol, -> (symbol) {
@@ -10,17 +12,23 @@ class Stock < ApplicationRecord
     avg_buy = average_buy(symbol)
     return if avg_buy.nil? || latest_price.nil?
 
-    ((avg_buy * quantity) - (latest_price * quantity)).round(2)
+    loss = (latest_price * self.quantity) - (avg_buy * self.quantity)
+    number_to_currency(loss)
   end
 
   def value
-    (latest_price * quantity).round(2)
+    number_to_currency(latest_price * self.quantity)
   end
 
   def latest_price
     @client = IEX::Api::Client.new
     @quote_data = @client.quote(symbol)
     latest_price = @quote_data.latest_price
+  end
+
+  def ave_buy
+    avg = average_buy(symbol)
+    number_to_currency(avg)
   end
 
   before_save :update_latest_price
@@ -35,10 +43,10 @@ class Stock < ApplicationRecord
     transactions = Transaction.where(symbol: symbol)
     return nil if transactions.empty?
 
-    total_price = transactions.sum(:price)
+    avg_total_amount = transactions.sum(:total_amount)
     total_quantity = self.quantity
   
-    total_price / total_quantity.to_f
+    avg_total_amount / total_quantity
   end
 
 end
