886c3ad3d2876e9c066a5648cc673a38bad25385
Paulene Pallan <paulenepallan@gmail.com>
2024-04-19 22:50:03 +0800

    feat: added services for fetching ohlc, historical prices, and logo

--- app/services/iex_stock_service.rb
+++ app/services/iex_stock_service.rb
@@ -10,4 +10,16 @@ class IexStockService
   def self.fetch_quote(symbol)
     @client.quote(symbol)
   end
+
+  def self.fetch_ohlc(symbol)
+    @client.ohlc(symbol)
+  end
+
+  def self.fetch_historical_prices(symbol)
+    @client.historical_prices(symbol, {range: '5d'})
+  end
+
+  def self.fetch_logo(symbol)
+    @client.logo(symbol)
+  end
 end
