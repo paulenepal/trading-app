3f32c99e930b9b2367e916aea1d308a613e2afa5
Marc Dimacuha <dimacuha.marc@gmail.com>
2024-04-22 19:17:54 +0800

    [CHORE] Refactor sell shares, and TransactionsController to include total_amount and total_shares in the response

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,7 +1,7 @@
 class TransactionsController < ApplicationController
   # before action: user auth [fr: applications controller]
 
-  # GET /transactions/index
+  # GET /transactions
   def index
     transactions = current_user.transactions.order(created_at: :desc)
     
@@ -31,11 +31,18 @@ class TransactionsController < ApplicationController
     transaction_params_price_merge = transaction_params.merge(price: @latest_price)
 
     transaction = Transaction.buy_shares!(current_user, transaction_params_price_merge)
+    total_amount = transaction_params[:quantity] * @latest_price
 
     if transaction
       render json: {
         status: { code: 200, message: 'Successfully added shares to assets' },
-        data: TransactionSerializer.new(transaction).serializable_hash[:data][:attributes]
+        data: {
+          summary: {
+            total_amount: total_amount,
+            total_shares: transaction_params[:quantity]
+          },
+          transaction: TransactionSerializer.new(transaction).serializable_hash[:data][:attributes]
+        }
       }
     else
       render json: { message: 'Failed to buy shares' }, status: :unprocessable_entity
@@ -51,8 +58,13 @@ class TransactionsController < ApplicationController
       return render json: { message: 'Insufficient shares to sell' }, status: :unprocessable_entity
     end
 
+    quote_data = IexStockService.fetch_quote(transaction_params[:symbol])
+    @latest_price = quote_data.latest_price
+
+    transaction_params_price_merge = transaction_params.merge(price: @latest_price)
+
     ## Sell shares
-    transaction = Transaction.sell_shares!(current_user, transaction_params)
+    transaction = Transaction.sell_shares!(current_user, transaction_params_price_merge)
 
     if transaction
       render json: {
