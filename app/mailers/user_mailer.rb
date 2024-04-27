20ce089444d7a065372a62605840a98fe8f95d3d
Paulene Pallan <paulenepallan@gmail.com>
2024-04-27 21:51:18 +0800

    refactor: updated user mailer default email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
-    mail(to: @user.email, subject: 'Your account has been approved') do |format|
+    mail(to: @user.email, subject: 'Your account has been approved!') do |format|
       format.html { render 'user_mailer/welcome' }
     end
   end
