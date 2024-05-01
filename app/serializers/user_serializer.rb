c0161e13de98d83ba798fb89dedbc6dc5bcfebaa
Paulene Pallan <paulenepallan@gmail.com>
2024-05-01 13:01:37 +0800

    feat: added balance on attributes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :id, :email, :first_name, :middle_name, :last_name, :username, :birthday, :confirmed_email, :role
+  attributes :id, :email, :first_name, :middle_name, :last_name, :username, :birthday, :confirmed_email, :role, :balance
 end
