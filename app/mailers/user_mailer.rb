1cf27a4bfb469e2d8f77738c010168a8b2876540
Paulene Pallan <paulenepallan@gmail.com>
2024-05-03 00:36:47 +0800

    feat: added login url

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
+    @login_url = 'https//localhost:3000/onboarding'
     mail(to: @user.email, subject: 'Your account has been approved!') do |format|
       format.html { render 'user_mailer/welcome' }
     end
