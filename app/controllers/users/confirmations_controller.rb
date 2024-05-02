b86fc813583fc7bdc38124af8788e07be1937917
Paulene Pallan <paulenepallan@gmail.com>
2024-05-03 00:36:09 +0800

    feat: added redirect link after email confirmation

---
+++ app/controllers/users/confirmations_controller.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class Users::ConfirmationsController < Devise::ConfirmationsController
+  # GET /resource/confirmation/new
+  # def new
+  #   super
+  # end
+
+  # POST /resource/confirmation
+  # def create
+  #   super
+  # end
+
+  # GET /resource/confirmation?confirmation_token=abcdef
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+
+    redirect_to 'http://localhost:3000/onboarding', allow_other_host: true
+  end
+
+  # protected
+
+  # The path used after resending confirmation instructions.
+  # def after_resending_confirmation_instructions_path_for(resource_name)
+  #   super(resource_name)
+  # end
+
+  # The path used after confirmation.
+  # def after_confirmation_path_for(resource_name, resource)
+  #   landing_url
+  # end
+end
