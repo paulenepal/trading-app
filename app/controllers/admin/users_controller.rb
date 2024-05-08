c26766b09bd26aad7bf89640b628c1ee71559a97
Paulene Pallan <paulenepallan@gmail.com>
2024-05-09 00:42:58 +0800

    feat: added minimum age restriction

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -33,19 +33,25 @@ class Admin::UsersController < ApplicationController
     new_user.password = Rails.application.credentials.user[:default_password]
     new_user.role = :trader # sets role to trader automatically
 
-    if new_user.save
-      new_user.update!(confirmed_at: Time.current)
-      UserMailer.welcome_email(new_user).deliver_now
-      render json: {
-        status: {code: 200, message: 'Signed up successfully.'},
-        data: UserSerializer.new(new_user).serializable_hash[:data][:attributes]
-      }
+    if new_user.minimum_age
+      if new_user.save
+        new_user.update!(confirmed_at: Time.current)
+        UserMailer.welcome_email(new_user).deliver_now
+        render json: {
+          status: {code: 200, message: 'Signed up successfully.'},
+          data: UserSerializer.new(new_user).serializable_hash[:data][:attributes]
+        }
+      else
+        render json: {
+          status: {message: "User couldn't be created successfully. #{new_user.errors.full_messages.to_sentence}"}
+        }, status: :unprocessable_entity
+      end
     else
       render json: {
-        status: {message: "User couldn't be created successfully. #{new_user.errors.full_messages.to_sentence}"}
+        status: {message: "User must be at least 18 years old."}
       }, status: :unprocessable_entity
     end
-
+  
   rescue StandardError => e
     render json: { error: 'An unexpected error occurred' }, status: :internal_server_error
   end
@@ -77,6 +83,6 @@ class Admin::UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email, :password, :first_name, :middle_name, :last_name, :username, :birthday, :confirmed_email, :role)
+    params.require(:user).permit(:email, :password, :first_name, :middle_name, :last_name, :username, :birthday, :confirmed_email, :role, :balance)
   end
 end
